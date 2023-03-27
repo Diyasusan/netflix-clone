f3b885f72cb0fa45a7e73a0b4a1ded3fc141f46e
Diyasusan <diyasusan97@gmail.com>
2023-03-28 00:27:03 +0530

    Profile screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import Homescreen from './Homescreen';
+import Homescreen from './screens/Homescreen';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
+import { auth } from './firbase';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from './features/userSlice'
+import { login } from './features/userSlice'
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+
+  useEffect(()=> {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if(userAuth){
+        //logged in
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
+      }
+      else {
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+  },[dispatch])
+ 
   return (
-    <div className="app">
-      <Homescreen/>
+    <div className="app">     
+    <BrowserRouter>
+    {!user ? (<LoginScreen />):(
+    <Routes>
+      <Route exact path="/profile" element={<ProfileScreen />}></Route>
+      <Route exact path="/" element={<Homescreen />}>
+      </Route>
+    </Routes>)}
+  </BrowserRouter>
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './Nav.css'
 
 function Nav() {
+
     const [show,handleShow] = useState(false);
+    
+    let navigate = useNavigate();
+
     const transitionNavBar = () => {
         if(window.scrollY > 100) {
             handleShow(true);
@@ -19,9 +24,14 @@ function Nav() {
   return (
     <div className= {`nav ${show && "nav__black"}`}>
         <div className='nav__contents'>
-        <img className='nav__logo'
+        <img onClick={ () => {
+            navigate('/');
+        } }
+        className='nav__logo'
         src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt=''/>
-        <img className='nav__avatar' 
+        <img className='nav__avatar' onClick={() => {
+            navigate('/profile');
+        }}
         src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117' alt=''/>
         </div>
     </div>
